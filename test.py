dd54d3bd60b51ede54ee7f02007aa93ecb2c4197
XavierMOP <527358495@qq.com>
2020-08-17 22:44:58 +0100

    mis

--- test.py
+++ test.py
@@ -13,3 +13,37 @@ def combination(c, d):
 
 pp = permutation(5, 5)
 cc = combination(32, 16)
+
+# %%
+
+import numpy as np
+import pandas as pd
+import warnings
+import sys
+import os
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+from bokeh.plotting import figure, show
+from bokeh.io import output_notebook
+from bokeh.layouts import row
+from sklearn.datasets.samples_generator import make_blobs
+
+# %%
+
+X, y = make_blobs(n_samples=300, centers=4, cluster_std=0.40, random_state=0)
+
+
+colormap = {0: 'red', 1: 'green', 2: 'blue', 3: 'yellow'}
+colors = [colormap[i] for i in y]
+
+plot_color = figure(plot_width=300, plot_height=300)
+plot_color.scatter(X[:, 0], X[:, 1], color=colors)
+
+plot_plain = figure(plot_width=300, plot_height=300)
+plot_plain.scatter(X[:, 0], X[:, 1])
+
+output_notebook()
+show(row(plot_plain, plot_color))
+
+# %%
